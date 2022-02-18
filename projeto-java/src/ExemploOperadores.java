3a38d67e0a48a8cca9f9d0a74f9ea08bc4f48bfe
Gleyson Sampaio <glysns@hotmail.com>
2022-02-18 13:39:15 -0300

    exemplo classes e objetos

---
+++ projeto-java/src/ExemploOperadores.java
@@ -0,0 +1,12 @@
+public class ExemploOperadores {
+    public static void main(String[] args) {
+        boolean processar = false;
+        if(processar){
+            System.out.println("processando");
+            System.out.println("calculando");
+            System.out.println("armazenando");
+        }
+       
+        System.out.println("fim");
+    }  
+}
