be5ed340e00ded2fa83022b0e75a5fe4ddecd037
Gleyson Sampaio <glysns@hotmail.com>
2022-02-21 19:34:40 -0300

    criando o projeto notification

--- projeto-java/src/ExemploOperadores.java
+++ projeto-java/src/ExemploOperadores.java
@@ -1,5 +1,6 @@
 public class ExemploOperadores {
     public static void main(String[] args) {
+        //https://strn.com.br/artigos/2018/12/11/todas-as-anota%C3%A7%C3%B5es-do-jpa-anota%C3%A7%C3%B5es-de-mapeamento/
         boolean processar = false;
         if(processar){
             System.out.println("processando");
