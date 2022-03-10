82b4e2733d594c6f1a15863d32f5564b6340a419
Gleyson Sampaio <glysns@hotmail.com>
2022-03-10 16:09:25 -0300

    teste

--- agua-luz-notificacao/src/main/java/mjv/al/notificacao/AguaLuzNotificacaoApp.java
+++ agua-luz-notificacao/src/main/java/mjv/al/notificacao/AguaLuzNotificacaoApp.java
@@ -2,6 +2,8 @@ package mjv.al.notificacao;
 
 import java.util.List;
 
+import com.google.gson.Gson;
+
 import mjv.al.atracao.model.Contrato;
 import mjv.al.atracao.service.ContratoService;
 import mjv.al.atracao.service.GeradorArquivo;
@@ -14,6 +16,9 @@ public class AguaLuzNotificacaoApp {
 		List<Contrato> contratos = leitor.lerArquivo();
 		GeradorArquivo gerador = new GeradorArquivo();
 		for(Contrato contrato: contratos) {
+			Gson gson = new Gson(); // conversor
+			String json = gson.toJson( contrato );
+			System.out.println(json);
 			System.out.println(contrato);
 			String conteudo = service.gerarConteudoContrato(contrato);
 			gerador.gerarContrato(conteudo, contrato.getPessoa().getCpf());
